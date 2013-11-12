b629e4142af415bf536b55390103803e9288daf1
Till Leinen <till.leinen@labyrint.com>
2013-11-12 22:24:21 +0100

    added coach registration form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Railsgirls::Application.routes.draw do
   get "static_pages/home"
   root  'static_pages#home'
   resources :sessions, only: [:new, :create, :destroy]
+  match '/registrations/new_coach', to: 'registrations#new_coach',  via: 'get'
   resources :registrations
   resources :users
   match '/admin',  to: 'sessions#new',         via: 'get'
