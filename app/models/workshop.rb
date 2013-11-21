690c7ec03bee2e71bb652a8b61fe71d197323e6a
johannesoliverengl <johannesoliver.engl@googlemail.com>
2013-11-21 15:53:34 +0100

    some changes on the workshop model

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,4 +1,4 @@
 class Workshop < ActiveRecord::Base
-	belongs_to :participant_form, :class_name => "Form", :associative_foreign_key => "participant_form_id"
-	belongs_to :coach_form, :class_name => "Form", :associative_foreign_key => "coach_form_id"
+	belongs_to :participant_form, :class_name => "Form"
+	belongs_to :coach_form, :class_name => "Form"
 end
