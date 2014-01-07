547cbf9455ab18ae08c4aaea8bbe0ad387e9be29
johannesoliverengl <johannesoliver.engl@googlemail.com>
2014-01-07 13:02:01 +0100

    language edits

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -2,5 +2,5 @@ class Settings
   include MongoMapper::Document
   key :logoURL,					String, :default => "railsgirls-logo.png"
   key :frontPageSlogan,			String, :default => "Welcome to Rails Girls"
-  key :frontPageDescription,		String, :default =>"Sie können sich zu folgenden Workshops anmelden:"
+  key :frontPageDescription,		String, :default =>"You can register for the following workshops:"
 end
