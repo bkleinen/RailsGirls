3ac8ef8542f1a2a7cde60583c25f5fdfb9c4ced5
johannesoliverengl <johannesoliver.engl@googlemail.com>
2014-01-17 00:10:44 +0100

    implemented filter for registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,34 +7,102 @@ class RegistrationsController < ApplicationController
   # GET /registrations
   # GET /registrations.json
   def index
-      registrations_data = Registration.all
-      @registrations = []
-      if registrations_data.count > 0
-        @form = Form.find(registrations_data.first.form_id)
-        if @form
-          @structure = JSON.parse @form.structure
-          hidden_keys = ["_id", "form_id", "form_type", "authenticity_token", "form_type", "action", "controller"]
-          registrations_data.each do |registration|
-            attributes = registration.attributes.clone
-            hidden_keys.each do |key|
-              attributes.delete key
-            end
-            i = 0
-            for elem in @structure
-              if elem["type"] == "radiobuttons"
-                val = attributes[elem["name"]]
-                attributes[elem["name"]] = elem["options"][val]
-              end
-              i += 1
-            end
-            reg = {}
-            reg["id"] = registration.id.to_s
-            reg["attributes"] = attributes
-            @registrations.push reg
-            print @structure
-          end
+    # render :text => "test"
+    @list_options = load_list_options
+    @previously_selected_workshop = params["workshop"]
+    @previously_selected_type = params["form_type"]
+    workshop = nil
+    registrations_data_participant = []
+    registrations_data_coach = []
+
+    if (!params["workshop"])
+      workshop = Workshop.find_by_status("published")
+    else
+      if params["workshop"] != "all"
+        workshop = Workshop.find(params["workshop"])
+      end
+    end
+    if params["workshop"] != "all"
+      if (workshop.participant_form? && params["form_type"] != "coach")
+        registrations_data_participant = workshop.participant_form.registrations
+      end
+      if (workshop.coach_form? && params["form_type"] != "participant" )
+        registrations_data_coach = workshop.coach_form.registrations
+      end
+    end
+
+    @participant_registrations = []
+    @participant_structure = nil
+    @coach_registrations = []
+    @coach_structure = nil
+
+    if workshop
+      if registrations_data_participant.count > 0
+          tmp = parse_structure(workshop.participant_form, registrations_data_participant)
+          @participant_registrations = tmp[0]
+          @participant_structure = tmp[1]
+      end
+      if registrations_data_coach
+          tmp = parse_structure(workshop.coach_form, registrations_data_coach)
+          @coach_registrations = tmp[0]
+          @coach_structure = tmp[1]
+      end
+    elsif params["workshop"]
+      @participant_structure = [{"type"=>"text", "caption"=>"Firstname", "name"=>"firstname", "class"=>"immutable_element", "id"=>"0"},
+        {"type"=>"text", "caption"=>"Lastname", "name"=>"lastname", "class"=>"immutable_element", "id"=>"1"},
+        {"type"=>"text", "caption"=>"E-Mail", "name"=>"email", "class"=>"immutable_element", "id"=>"2"}]
+      if params["form_type"] == "all"
+        Registration.all.each do |registration|
+          reg = {"id"=>registration.id, "attributes"=>
+            {"firstname"=>registration.attributes["firstname"],
+            "lastname"=>registration.attributes["lastname"],
+            "email"=>registration.attributes["email"]}}
+            @participant_registrations.push(reg)
+        end
+      elsif params["form_type"] == "participant"
+        Registration.find_all_by_form_type("ParticipantForm").each do |registration|
+          reg = {"id"=>registration.id, "attributes"=>
+            {"firstname"=>registration.attributes["firstname"],
+            "lastname"=>registration.attributes["lastname"],
+            "email"=>registration.attributes["email"]}}
+            @participant_registrations.push(reg)
+        end
+      else
+        Registration.find_all_by_form_type("CoachForm").each do |registration|
+          reg = {"id"=>registration.id, "attributes"=>
+            {"firstname"=>registration.attributes["firstname"],
+            "lastname"=>registration.attributes["lastname"],
+            "email"=>registration.attributes["email"]}}
+            @participant_registrations.push(reg)
         end
       end
+    end
+  end
+
+  def parse_structure(form, registrations_data)
+    registrations = []
+    structure = JSON.parse form.structure
+    hidden_keys = ["_id", "form_id", "form_type", "authenticity_token", "form_type", "action", "controller"]
+    registrations_data.each do |registration|
+      attributes = registration.attributes.clone
+      hidden_keys.each do |key|
+        attributes.delete key
+      end
+      i = 0
+      for elem in structure
+        if elem["type"] == "radiobuttons"
+          val = attributes[elem["name"]]
+          attributes[elem["name"]] = elem["options"][val]
+        end
+        i += 1
+      end
+
+      reg = {}
+      reg["id"] = registration.id.to_s
+      reg["attributes"] = attributes
+      registrations.push reg
+    end
+    structure_and_registration = [registrations, structure]
   end
 
   # GET /registrations/1
@@ -103,6 +171,15 @@ class RegistrationsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_registration
       @registration = Registration.find(params[:id])
+    end
+
+    def load_list_options
+      workshop_options = []
+      workshop_options.push(["all", "all"])
+      Workshop.all.each do |workshop|
+        workshop_options.push([workshop.name + " : " + workshop.date.to_s, Workshop.find(workshop.id).id])
+      end
+      options = {:workshop => workshop_options, :form_type => ["all", "participant", "coach"]}
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -10,6 +10,5 @@ class Registration
 	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	# validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
  # 	                   uniqueness: { case_sensitive: false }
-
 	belongs_to :form, :polymorphic => true
 end
