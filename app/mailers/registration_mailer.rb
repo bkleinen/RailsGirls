d4ac70ef3b8c6c138a973338f6a739557115f388
johannesoliverengl <johannesoliver.engl@googlemail.com>
2014-01-18 22:40:18 +0100

    bug fix send manual mails

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -16,29 +16,35 @@ class RegistrationMailer < ActionMailer::Base
 
 	def manual_email(mail_details)
 		participant_email_with_name = "Rails Girls <railsgirlsmanagement@gmail.com>"
+		workshop = Workshop.find(mail_details[:workshop][:id])
 		receipments = []
-		if mail_details[:admin]
+		if mail_details["admins"]
 			User.all.each do |user|
 				receipments.push user.email
 			end
 		end
-		if mail_details[:participants]
-			Registration.all.each do |registration|
-				if registration.form_type == "ParticipantForm"
-					receipments.push registration.email
-				end
+		if mail_details["participants"]
+			workshop.participant_form.registrations.each do |registration|
+				receipments.push registration.email
 			end
 		end
-		if mail_details[:coach]
-			Registration.all.each do |registration|
-				if registration.form_type == "CoachForm"
-					receipments.push registration.email
-				end
+		if mail_details["coach"]
+			workshop.coach_form.registrations.each do |registration|
+				receipments.push registration.email
+			end
+		end
+		if mail_details["participants_accepted"]
+			workshop.participant_form.registrations.find_all_by_accepted(true).each do |registration|
+				receipments.push registration.email
+			end
+		end
+		if mail_details["participants_rejected"]
+			workshop.participant_form.registrations.all(:accepted.ne => true).each do |registration|
+				receipments.push registration.email
 			end
 		end
 		mail_text = mail_details[:text]
 		if mail_details[:text]
-			workshop = Workshop.find(mail_details[:workshop][:id])
 			pseudo_tags = {
 				"[workshop_name]" => workshop.name,
 				"[workshop_description]" => workshop.description,
@@ -49,7 +55,7 @@ class RegistrationMailer < ActionMailer::Base
 				mail_text.gsub! key, value
 			end
 		end
-		mail(to: participant_email_with_name,
+		mail(to: 'railsgirlsmanagement@gmail.com',
 			bcc: receipments,
 			subject: mail_details[:subject],
 			body: mail_text,
